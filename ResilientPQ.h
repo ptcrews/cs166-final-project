6ecb5243ecae5ac1b5010af543a0553569945fe1
Ashwini Pokle <ashwini1@myth66.stanford.edu>
2019-05-30 17:26:35 -0700

    Added Layers to ResilientPQ

--- ResilientPQ.h
+++ ResilientPQ.h
@@ -16,6 +16,8 @@ private:
   vector<int> buffer;
   size_t buffer_threshold;
 
+  vector<Layer> layers;
+
     /* Fun with C++: these next two lines disable implicitly-generated copy
    * functions that would otherwise cause weird errors if you tried to
    * implicitly copy an object of this type. You don't need to touch these
